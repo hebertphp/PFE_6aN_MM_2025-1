3d9733d0aedc6be1de410e636111e11b5e4b81c5
Prof. Hebert <hebertphp@gmail.com>
2025-02-28 21:36:04 -0300

    Add files via upload

---
+++ JS04/script.js
@@ -0,0 +1,10 @@
+document.write("Carnaval!<br>");
+let nome="Bete";
+let idade = 35;
+let nota = 7.8;
+document.write("Seu nome é "+nome);
+idade=40;
+document.write("<br>Sua idade é "+idade);
+document.write("<br>Sua nota foi "+nota+"<br>");
+document.write(nome+", você tem "+idade+" anos e tirou "+nota)
+//Bete, você tem 35 anos e tirou 7.8
